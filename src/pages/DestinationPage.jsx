960f11554f94a652cb003108d1c6879ea6d9f628
shahaf noked <shahafnoked@gmail.com>
2024-09-26 18:14:16 +0300

    added transition for destination data

--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function DestinationPage({ dest }) {
   const [idx, setIdx] = useState(0);
@@ -11,34 +12,66 @@ export default function DestinationPage({ dest }) {
           <span>01</span>Pick your destination{" "}
         </h2>
         <div className="destination">
-          <div className="img-container">
-            <img src={data.images.png} alt="destination image" />
-          </div>
+          <TransitionGroup>
+            <CSSTransition key={idx} classNames="fade" timeout={500}>
+              <div className="img-container">
+                <img src={data.images.png} alt="destination image" />
+              </div>
+            </CSSTransition>
+          </TransitionGroup>
 
           <div className="content-container">
-
             <div className="destination-nav text-8">
-              <a href="#moon" onClick={() => setIdx(0)} className={(idx === 0 ? 'active' : '')}>Moon</a>
-              <a href="#mars" onClick={() => setIdx(1)} className={(idx === 1 ? 'active' : '')}>Mars</a>
-              <a href="#europa" onClick={() => setIdx(2)} className={(idx === 2 ? 'active' : '')}>Europa</a>
-              <a href="#titan" onClick={() => setIdx(3)} className={(idx === 3 ? 'active' : '')}>Titan</a>
+              <a
+                href="#moon"
+                onClick={() => setIdx(0)}
+                className={idx === 0 ? "active" : ""}
+              >
+                Moon
+              </a>
+              <a
+                href="#mars"
+                onClick={() => setIdx(1)}
+                className={idx === 1 ? "active" : ""}
+              >
+                Mars
+              </a>
+              <a
+                href="#europa"
+                onClick={() => setIdx(2)}
+                className={idx === 2 ? "active" : ""}
+              >
+                Europa
+              </a>
+              <a
+                href="#titan"
+                onClick={() => setIdx(3)}
+                className={idx === 3 ? "active" : ""}
+              >
+                Titan
+              </a>
             </div>
 
-            <h1 className="text-2">{data.name}</h1>
-            <p className="text-9">{data.description}</p>
-            <div className="line"></div>
+            <TransitionGroup>
+              <CSSTransition key={idx} classNames="fade" timeout={500}>
+                <div>
+                  <h1 className="text-2">{data.name}</h1>
+                  <p className="text-9">{data.description}</p>
+                  <div className="line"></div>
 
-            <div className="info">
-              <div className="distance">
-                <p className="text-7">Avg. distance</p>
-                <p className="text-6">{data.distance}</p>
-              </div>
-              <div className="time">
-                <p className="text-7">Est. travel time</p>
-                <p className="text-6">{data.travel}</p>
-              </div>
-            </div>
-
+                  <div className="info">
+                    <div className="distance">
+                      <p className="text-7">Avg. distance</p>
+                      <p className="text-6">{data.distance}</p>
+                    </div>
+                    <div className="time">
+                      <p className="text-7">Est. travel time</p>
+                      <p className="text-6">{data.travel}</p>
+                    </div>
+                  </div>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
           </div>
         </div>
       </div>
