4ae10e5515b714128517c7da2b2238c1782fbb6e
shahaf noked <shahafnoked@gmail.com>
2024-09-26 15:38:38 +0300

    desktop navbar and home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
+import {NavLink } from "react-router-dom";
 import Logo from './../assets/icons/logo.svg?react';
-import Hamburger from './../assets/icons/icon-hamburger.svg?react';
+// import Hamburger from './../assets/icons/icon-hamburger.svg?react';
 
 export default function NavBar() {
   return (
     <div className="navbar-container">
       <div className="logo"><Logo/></div>
       <div className="line"></div>
-      <nav>
-        <Link to="/"></Link>
-        <Link to="/home"><span>00</span> Home</Link>
-        <Link to="/crew"><span>01</span> Crew</Link>
-        <Link to="/dest"><span>02</span> Destination</Link>
-        <Link to="/tech"><span>03</span> Technology</Link>
+      <nav className="text-8">
+        <NavLink to="/"></NavLink>
+        <NavLink to="/"><span>00</span> Home</NavLink>
+        <NavLink to="/dest"><span>01</span> Destination</NavLink>
+        <NavLink to="/crew"><span>02</span> Crew</NavLink>
+        <NavLink to="/tech"><span>03</span> Technology</NavLink>
       </nav>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,24 @@
-
 export default function HomePage() {
   return (
     <div className="home-container page">
-      Home Page Container
-    </div>
-  )
-}
+      <div className="center-content">
+        <div className="content">
+          <div className="text">
+            <h3 className="text-5">So, you want to travel to</h3>
+            <h1 className="text-1">Space</h1>
+            <p className="text-9">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className="button-container">
+            <button className="text-4">Explore</button>
+          </div>
 
+        </div>
+      </div>
+    </div>
+  );
+}
