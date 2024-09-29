bee10f9d3a22b8a02cbf23eb2e9be205c230a7f1
shahaf noked <shahafnoked@gmail.com>
2024-09-29 14:36:42 +0300

    replace react-transition with framer motion

--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function DestinationPage({ dest }) {
   const [idx, setIdx] = useState(0);
@@ -11,69 +11,62 @@ export default function DestinationPage({ dest }) {
         <h2 className="text-5">
           <span>01</span>Pick your destination{" "}
         </h2>
-        <div className="destination">
-          <TransitionGroup>
-            <CSSTransition key={idx} classNames="fade" timeout={500}>
-              <div className="img-container">
-                <img src={data.images.png} alt="destination image" />
-              </div>
-            </CSSTransition>
-          </TransitionGroup>
-
-          <div className="content-container">
-            <div className="destination-nav text-8">
-              <a
-                href="#moon"
-                onClick={() => setIdx(0)}
-                className={idx === 0 ? "active" : ""}
-              >
-                Moon
-              </a>
-              <a
-                href="#mars"
-                onClick={() => setIdx(1)}
-                className={idx === 1 ? "active" : ""}
-              >
-                Mars
-              </a>
-              <a
-                href="#europa"
-                onClick={() => setIdx(2)}
-                className={idx === 2 ? "active" : ""}
-              >
-                Europa
-              </a>
-              <a
-                href="#titan"
-                onClick={() => setIdx(3)}
-                className={idx === 3 ? "active" : ""}
-              >
-                Titan
-              </a>
+        <AnimatePresence mode="wait">
+          <div className="destination">
+            <div className="img-container">
+              <motion.img src={data.images.png} alt="destination image" key={data.images.png} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0}} transition={{ duration: 0.5, ease: "easeInOut" }}/>
             </div>
+            <div className="content-container">
+              <div className="destination-nav text-8">
+                <a
+                  href="#moon"
+                  onClick={() => setIdx(0)}
+                  className={idx === 0 ? "active" : ""}
+                >
+                  Moon
+                </a>
+                <a
+                  href="#mars"
+                  onClick={() => setIdx(1)}
+                  className={idx === 1 ? "active" : ""}
+                >
+                  Mars
+                </a>
+                <a
+                  href="#europa"
+                  onClick={() => setIdx(2)}
+                  className={idx === 2 ? "active" : ""}
+                >
+                  Europa
+                </a>
+                <a
+                  href="#titan"
+                  onClick={() => setIdx(3)}
+                  className={idx === 3 ? "active" : ""}
+                >
+                  Titan
+                </a>
+              </div>
 
-            <TransitionGroup>
-              <CSSTransition key={idx} classNames="fade" timeout={500}>
-                <div>
-                  <h1 className="text-2">{data.name}</h1>
-                  <p className="text-9">{data.description}</p>
-                  <div className="line"></div>
+              <motion.div key={data.name} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0}} transition={{ duration: 0.5, ease: "easeInOut" }}>
+                <h1 className="text-2">{data.name}</h1>
+                <p className="text-9">{data.description}</p>
+                <div className="line"></div>
 
-                  <div className="info">
-                    <div className="distance">
-                      <p className="text-7">Avg. distance</p>
-                      <p className="text-6">{data.distance}</p>
-                    </div>
-                    <div className="time">
-                      <p className="text-7">Est. travel time</p>
-                      <p className="text-6">{data.travel}</p>
-                    </div>
+                <div className="info">
+                  <div className="distance">
+                    <p className="text-7">Avg. distance</p>
+                    <p className="text-6">{data.distance}</p>
+                  </div>
+                  <div className="time">
+                    <p className="text-7">Est. travel time</p>
+                    <p className="text-6">{data.travel}</p>
                   </div>
                 </div>
-              </CSSTransition>
-            </TransitionGroup>
+              </motion.div>
+            </div>
           </div>
-        </div>
+        </AnimatePresence>
       </div>
     </div>
   );
