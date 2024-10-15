cc0fdbd4af6ee36dd69cfecdc02c356de11fa33c
shahaf noked <shahafnoked@gmail.com>
2024-10-15 19:05:27 +0300

    navbar open close

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,29 @@
 import {NavLink, useNavigate } from "react-router-dom";
 import Logo from './../assets/icons/logo.svg?react';
-// import Hamburger from './../assets/icons/icon-hamburger.svg?react';
+import Hamburger from './../assets/icons/icon-hamburger.svg?react';
+import CloseIcon from './../assets/icons/icon-close.svg?react';
+import { useState } from "react";
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate()
+
+  const closeNav = () => {
+    setIsOpen(false)
+  }
+
   return (
     <div className="navbar-container">
       <div className="logo" onClick={() => navigate('/')}><Logo/></div>
       <div className="line"></div>
-      <nav className="text-8">
+      <nav className={isOpen? "text-8 open" : 'text-8'} onClick={closeNav}>
         <NavLink to="/"></NavLink>
         <NavLink to="/"><span>00</span> Home</NavLink>
         <NavLink to="/destination"><span>01</span> Destination</NavLink>
         <NavLink to="/crew"><span>02</span> Crew</NavLink>
         <NavLink to="/technology"><span>03</span> Technology</NavLink>
       </nav>
+      <div className="hamburger" onClick={() => setIsOpen(() => !isOpen)}>{ !isOpen? <Hamburger/> : <CloseIcon/> }</div>
     </div>
   );
 }
