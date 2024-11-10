3881286287ec68ae6733373acf77a278ad74481d
shahaf noked <shahafnoked@gmail.com>
2024-11-10 17:35:33 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),svgr()],
-  // base: '/Interactive-card-form/', change on deploy
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), svgr()],
+    base: mode === 'production' ? '/space-tourism/' : '/',
+  };
+});
